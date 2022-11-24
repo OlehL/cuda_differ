241a24889e0114a88a6754d97469d952531ca0c2
Alexey <support@uvviewsoft.com>
2022-11-24 23:23:37 +0300

    limit cnt of tab menu items

--- __init__.py
+++ __init__.py
@@ -606,7 +606,7 @@ class Command:
                 caption=_('Compare with tab')
                 )
 
-        handles = ct.ed_handles()
+        handles = ct.ed_handles()[:20] # avoid too much menu items when user opens 100 files
         cur_fn = cur_ed.get_filename()
         paths = []
         if len(handles) > 1:
