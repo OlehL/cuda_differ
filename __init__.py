9fcaf8ec74e97771ba9cdc6e85fb04a818682928
Alexey <support@uvviewsoft.com>
2022-12-28 17:40:05 +0300

    jump prev fix

--- __init__.py
+++ __init__.py
@@ -473,11 +473,12 @@ class Command:
     def jump(self, to_next=True):
         if not self.diff.diffmap:
             self.refresh()
-        if len(self.diff.diffmap) == 0:
+        cnt = len(self.diff.diffmap)
+        if cnt == 0:
             return ct.msg_status(_("No differences were found"))
         fc, eds = self.focused
 
-        i = None
+        i = None if to_next else cnt - 2 # second to last index is the default when going back
         if fc == 0:
             p = 0 if to_next else 1
         else:
@@ -489,11 +490,11 @@ class Command:
                 break
 
         if i is None:
-            i = 0 if to_next else len(self.diff.diffmap) - 1
-        elif i >= len(self.diff.diffmap):
+            i = 0 if to_next else cnt - 1
+        elif i >= cnt:
             i = 0
         elif i < 0:
-            i = len(self.diff.diffmap) - 1
+            i = cnt - 1
         to = self.diff.diffmap[i]
         eds[0].set_caret(0, to[0], 0, to[1], ct.CARET_SET_ONE)
         eds[1].set_caret(0, to[2], 0, to[3], ct.CARET_SET_ONE)
